43a2d3433a0fdc7a519c9e6d34a0fe8921ddc821
Austin Bayley <austin.bayley@icloud.com>
2025-05-22 19:49:12 +0100

    chore: fmt

--- infra/modules/cloudflare/zone.tf
+++ infra/modules/cloudflare/zone.tf
@@ -2,6 +2,6 @@ resource "cloudflare_zone" "austinbayley" {
   name = "austinbayley.co.uk"
   type = "full"
   account = {
-    id   = "39006b4c125bbd02355ed1cd3a7bb22f"
+    id = "39006b4c125bbd02355ed1cd3a7bb22f"
   }
 }
