66f514540ea2fda03a95fd557b1178ff4327c6d1
Austin Bayley <austin.bayley@icloud.com>
2025-05-26 09:00:15 +0100

    chore: switch to dynamic vault ID

--- infra/modules/oracle/outputs.tf
+++ infra/modules/oracle/outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "Public IP of the Oracle instance"
   value       = oci_load_balancer_load_balancer.k3s_lb.ip_address_details[0].ip_address
 }
+
+output "vault_id" {
+  description = "ID of the secerts vault"
+  value       = oci_kms_vault.secret_vault.id
+}
