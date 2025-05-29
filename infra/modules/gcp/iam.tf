4b932eecc2e80aab6bb3eb1958d85268b64c9d32
Austin Bayley <austin.bayley@icloud.com>
2025-05-29 20:18:02 +0100

    chore: setup workload identity federation

--- infra/modules/gcp/iam.tf
+++ infra/modules/gcp/iam.tf
@@ -1,11 +1,12 @@
-resource "google_service_account" "github_actions" {
-  account_id   = "github-actions"
-  display_name = "Github Actions Service Account"
-}
-
 resource "google_service_account" "kubernetes" {
   account_id   = "kubernetes"
   display_name = "Kubernetes Service Account"
+}
+
+resource "google_service_account_iam_member" "workload_identity_binding" {
+  service_account_id = google_service_account.kubernetes.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${google_service_account.kubernetes.email}"
 }
 
 # ==========
@@ -24,3 +25,23 @@ resource "google_project_service" "sts" {
   disable_on_destroy = false
 }
 
+resource "google_iam_workload_identity_pool" "kubernetes" {
+  workload_identity_pool_id = "kubernetes"
+  display_name              = "Kubernetes"
+  description               = "Portfolio cluster workload identity pool"
+}
+
+resource "google_iam_workload_identity_pool_provider" "example" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.kubernetes.workload_identity_pool_id
+  workload_identity_pool_provider_id = "cluster"
+  display_name                       = "Cluster"
+  description                        = "Workload identity provider for Kubernetes cluster"
+
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+  }
+
+  oidc {
+    issuer_uri = "https://kube.austinbayley.co.uk"
+  }
+}
