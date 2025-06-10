bf0dd9dbdeb201dbd8b1caa5cccacf64b2978a32
Austin Bayley <austin.bayley@icloud.com>
2025-06-10 20:30:26 +0100

    chore: add Actions

--- infra/modules/gcp/iam.tf
+++ infra/modules/gcp/iam.tf
@@ -9,6 +9,23 @@ resource "google_service_account_iam_member" "kubernetes_workload_identity_bindi
   member             = "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.kubernetes.workload_identity_pool_id}/subject/system:serviceaccount:default:default"
 }
 
+resource "google_service_account" "github_actions" {
+  account_id   = "github-actions"
+  display_name = "Github Actions Service Account"
+}
+
+resource "google_service_account_iam_member" "github_actions_workload_identity_binding" {
+  service_account_id = google_service_account.github_actions.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github.workload_identity_pool_id}/attribute.repository_owner/abayleypublic"
+}
+
+resource "google_project_iam_member" "github_actions_artifact_writer" {
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
 resource "google_service_account" "image_pull" {
   account_id   = "image-pull"
   display_name = "Image Pull Service Account"
@@ -48,7 +65,7 @@ resource "google_iam_workload_identity_pool" "kubernetes" {
   description               = "Portfolio cluster workload identity pool"
 }
 
-resource "google_iam_workload_identity_pool_provider" "example" {
+resource "google_iam_workload_identity_pool_provider" "cluster" {
   workload_identity_pool_id          = google_iam_workload_identity_pool.kubernetes.workload_identity_pool_id
   workload_identity_pool_provider_id = "cluster"
   display_name                       = "Cluster"
@@ -61,4 +78,29 @@ resource "google_iam_workload_identity_pool_provider" "example" {
   oidc {
     issuer_uri = "https://kube.austinbayley.co.uk"
   }
+}
+
+resource "google_iam_workload_identity_pool" "github" {
+  workload_identity_pool_id = "github"
+  display_name              = "Github"
+  description               = "Github workload identity pool"
+}
+
+resource "google_iam_workload_identity_pool_provider" "actions" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  workload_identity_pool_provider_id = "actions"
+  display_name                       = "Actions"
+  description                        = "Workload identity provider for GitHub Actions"
+
+  attribute_mapping = {
+    "google.subject"             = "assertion.sub"
+    "attribute.repository"       = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+  }
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+
+  attribute_condition = "assertion.repository_owner == 'abayleypublic'"
 }
