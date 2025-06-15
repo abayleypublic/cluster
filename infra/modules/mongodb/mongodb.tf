0147407e3e3f8124e63451817c42f4ad1ecc60f8
Austin Bayley <austin.bayley@icloud.com>
2025-06-15 09:32:04 +0100

    chore: cleanup

--- infra/modules/mongodb/mongodb.tf
+++ infra/modules/mongodb/mongodb.tf
@@ -19,9 +19,3 @@ resource "mongodbatlas_advanced_cluster" "cluster" {
     }
   }
 }
-
-resource "mongodbatlas_project_ip_access_list" "oci_nat" {
-  project_id = mongodbatlas_project.portfolio.id
-  ip_address = var.cluster_egress_ip
-  comment    = "Cluster Egress IP"
-}
